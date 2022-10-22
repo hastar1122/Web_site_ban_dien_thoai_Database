6e690d471b83172eb9496cbc031f679923b31a4f
NguyenHoangAnh <hoanganh08102000@gmail.com>
2022-10-22 15:09:31 +0700

    Sửa lại database

--- Data/user.sql
+++ Data/user.sql
@@ -1,3 +1,3 @@
-INSERT INTO quan_ly_website_ban_dien_thoai.user(UserID, UserAccount, Password, UserName, Address, Email, PhoneNumber, Image, RoleID) VALUES
+﻿INSERT INTO quan_ly_website_ban_dien_thoai.user(UserID, UserAccount, Password, UserName, Address, Email, PhoneNumber, Image, RoleID) VALUES
 (1, 'truonghieu', '123', 'Truong Minh Hieu', 'Thai Binh', 'truongminhhieu152@gmail.com', '0392417241', 'a.jpg', 1),
-(2, 'admin', '12345', 'Minh Hiếu', 'Hà Nội', 'hieu110501@gmail.com', '099993213', 'x.jpg', 2),
+(2, 'admin', '12345', 'Minh Hiếu', 'Hà Nội', 'hieu110501@gmail.com', '099993213', 'x.jpg', 2);
