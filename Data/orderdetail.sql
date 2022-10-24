00929011f01a9f1b8854c05cdeb8493c33d1e1f4
DESKTOP-CEHRP21\kimda <nguyenkimdat2307@gmail.com>
2022-10-24 18:01:37 +0700

    thêm dữ liệu

---
+++ Data/orderdetail.sql
@@ -0,0 +1,2 @@
+INSERT INTO orderdetail (OrderDetailID, OrderID, ProductID, Amount, Price) VALUES
+(1, 1, 3, 1, 10000);
--- Data/role.sql
+++ Data/role.sql
@@ -1,3 +1,4 @@
 INSERT INTO quan_ly_website_ban_dien_thoai.role(RoleID, RoleName) VALUES
 (1, 'employee'),
-(2, 'admin');
+(2, 'admin'),
+(3, 'Khach Hang');
