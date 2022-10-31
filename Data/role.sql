1ea35f23eff6e8a95c17bc5263bd2838d0bc259e
Nguyen2 <Ngo2uHuy@gmail.com>
2022-10-31 15:12:57 +0700

    Insert role

--- Data/role.sql
+++ Data/role.sql
@@ -1,4 +1,1 @@
-INSERT INTO quan_ly_website_ban_dien_thoai.role(RoleID, RoleName) VALUES
-(1, 'employee'),
-(2, 'admin'),
-(3, 'Khach Hang');
+INSERT INTO quan_ly_website_ban_dien_thoai.role(RoleID, RoleName) VALUES('supplier'),('customer')
