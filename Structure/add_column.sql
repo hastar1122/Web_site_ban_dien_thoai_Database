d900b9fb48d20e7e17d13a6bbaaf019641208f9b
NguyenHoangAnh <hoanganh08102000@gmail.com>
2022-11-03 12:09:17 +0700

    Thêm cột mã đơn hàng, mã hóa đơn nhâp, giá bán, giá nhập

---
+++ Structure/add_column.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE `order` ADD COLUMN OrderCode varchar(255) DEFAULT NULL COMMENT 'Mã hóa đơn' AFTER OrderID;
+ALTER TABLE orderdetail ADD COLUMN Price float DEFAULT NULL COMMENT 'Giá bán' AFTER ProductID;
+ALTER TABLE importbill ADD COLUMN ImportBillCode varchar(255) DEFAULT NULL COMMENT 'Mã hóa đơn nhập' AFTER ImportBillID;
+ALTER TABLE importbilldetail ADD COLUMN Price float DEFAULT NULL COMMENT 'Giá nhập' AFTER ProductID;
